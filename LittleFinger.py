98877a8bc5b03d0ecfcfece154b787c006097b9d
Lucas Rival <lucas.rival@gmail.com>
2016-02-23 10:59:41 +0100

    Added function for find email and phone number from a webpage

--- LittleFinger.py
+++ LittleFinger.py
@@ -3,6 +3,52 @@
 
 import os,subprocess,sys,re,json
 
+#####################################################################################
+# Fonction qui permet de chercher dans une page donnée tout les email               #
+# @param url : l'URL a parcourir                                                    #
+# @return phoneNumList : la liste de tout les email trouvé                          #
+#####################################################################################
+def findEmail(url):
+    emailList=[]
+    # req=urllib2.Request(sys.argv[1])
+    fd=urllib2.urlopen(url)
+    while 1:
+        data=fd.read(1024)
+        email=re.search('[a-z0-9._\-]+\@[a-z.\-]*',data)
+        if email:
+            emailList.append(email.group(0))
+        if not len(data):
+            break
+    for email in emailList:
+        if emailList.count(email)>1:
+            emailList.remove(email)
+    emailList.sort()
+    return emailList
+
+#####################################################################################
+# Fonction qui permet de chercher dans une page donnée tout les numéro de téléphone #
+# @param url : l'URL a parcourir                                                    #
+# @return phoneNumList : la liste de tout les numéro trouvé                         #
+#####################################################################################
+def findPhoneNumber(url):
+    phoneNumList=[]
+    # req=urllib2.Request(sys.argv[1])
+    fd=urllib2.urlopen(url)
+    while 1:
+        data=fd.read(1024)
+        phoneNum=re.search('(0|\+33)[1-9]([-.\ ]?[0-9]{2}){4}',data)
+        if phoneNum:
+            phoneNumList.append(phoneNum.group(0))
+        if not len(data):
+            break
+    for phone in phoneNumList:
+        if phoneNumList.count(phone)>1:
+            phoneNumList.remove(phone)
+    phoneNumList.sort()
+    return phoneNumList
+
+
+    
 domain=sys.argv[1]
 dico={}
 print "Début de la prise d'empreinte sur",domain
@@ -50,3 +96,9 @@ for key in dico:
         dico[key]=''
 dico=dicotmp
 print dico
+
+for machine in dico:
+    if subprocess.check_output(['ping','-c','1',dico[machine]]):
+        print dico[machine],'is up'
+    else:
+        print dico[machine], 'is off'
